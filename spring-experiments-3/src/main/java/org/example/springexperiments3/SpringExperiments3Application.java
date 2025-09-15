a42a055cd18df0a077eed2ed82c1950d49f260bb
heming-jd <yhm050227@qq.com>
2025-09-16 07:53:40 +0800

    项目结构

---
+++ spring-experiments-3/src/main/java/org/example/springexperiments3/SpringExperiments3Application.java
@@ -0,0 +1,13 @@
+package org.example.springexperiments3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExperiments3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExperiments3Application.class, args);
+    }
+
+}
---
+++ spring-experiments-3/src/test/java/org/example/springexperiments3/SpringExperiments3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springexperiments3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExperiments3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
